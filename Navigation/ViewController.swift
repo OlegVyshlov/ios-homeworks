2c915c6da53a23e6fb7c7ef5340df01329dc42a9
Oleg Vyshlov <vyshlov@gmail.com>
2022-09-29 21:04:27 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by OV on 29.09.2022.
+//  Copyright © 2022 OV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
